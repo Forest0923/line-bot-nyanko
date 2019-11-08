0398a7f0b343f45f20a291fd11f5d9a319e72d23
forest0923 <tropicalrainforest23@gmail.com>
2019-11-08 21:04:09 +0900

    add README.md, modified index.js

--- index.js
+++ index.js
@@ -28,40 +28,69 @@ app.post('/', line.middleware(config), (req, res) => {
 
 
 function handleEvent(event){
-  let replyText = {
-    type: 'text',
-    text:  'Hi :)\n'
-  }
-  switch (event.message.type){
-  case 'text':
-    replyText.text += 'I got TEXT'
-    return client.replyMessage(event.replyToken, replyText);
-    
-  case 'image':
-    replyText.text += 'I got IMAGE'
-    return client.replyMessage(event.replyToken, replyText);
-    
-  case 'video':
-    replyText.text += 'I got VIDEO'
-    return client.replyMessage(event.replyToken, replyText);
+  switch(event.type){
+  case 'message':
+    let replyText = {
+      type: 'text',
+      text:  'Hi :)\n'
+    }
+    switch(event.message.type){
+    case 'text':
+      replyText.text += 'I got TEXT'
+      return client.replyMessage(event.replyToken, replyText);
+      
+    case 'image':
+      replyText.text += 'I got IMAGE'
+      return client.replyMessage(event.replyToken, replyText);
+      
+    case 'video':
+      replyText.text += 'I got VIDEO'
+      return client.replyMessage(event.replyToken, replyText);
 
-  case 'audio':
-    replyText.text += 'I got AUDIO'
-    return client.replyMessage(event.replyToken, replyText);
+    case 'audio':
+      replyText.text += 'I got AUDIO'
+      return client.replyMessage(event.replyToken, replyText);
+      
+    case 'file':
+      replyText.text += 'I got FILE'
+      return client.replyMessage(event.replyToken, replyText);
+      
+    case 'location':
+      replyText.text += 'I got LOCATION'
+      return client.replyMessage(event.replyToken, replyText);
+      
+    case 'sticker':
+      replyText.text += 'I got STICKER'
+      return client.replyMessage(event.replyToken, replyText);
+      
+    default:
+      return Promise.resolve(null);
+    }
     
-  case 'file':
-    replyText.text += 'I got FILE'
-    return client.replyMessage(event.replyToken, replyText);
+  case 'follow':
+    return client.replyMessage(event.replyToken, {type: 'text', text: 'Got followed event'});
     
-  case 'location':
-    replyText.text += 'I got LOCATION'
-    return client.replyMessage(event.replyToken, replyText);
+  case 'unfollow':
+    return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+
+  case 'join':
+    return client.replyMessage(event.replyToken, {type: 'text', text: `Joined ${event.source.type}`});
     
-  case 'sticker':
-    replyText.text += 'I got STICKER'
-    return client.replyMessage(event.replyToken, replyText);
+  case 'leave':
+    return console.log(`Left: ${JSON.stringify(event)}`);
+    
+  case 'postback':
+    let data = event.postback.data;
+    if (data === 'DATE' || data === 'TIME' || data === 'DATETIME') {
+      data += `(${JSON.stringify(event.postback.params)})`;
+    }
+    return client.replyMessage(event.replyToken, {type: 'text', text: `Got postback: ${data}`});
+    
+  case 'beacon':
+    return client.replyMessage(event.replyToken, {type: 'text', text: `Got beacon: ${event.beacon.hwid}`});
+    
   default:
-    return Promise.resolve(null);
+    throw new Error(`Unknown event: ${JSON.stringify(event)}`);
   }
 }
 
